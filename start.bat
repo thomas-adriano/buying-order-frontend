44cdea7bb7a2ee745fbe5cb7520dbc9d78727152
Thomas O. Adriano <thomas.o.adriano@gmail.com>
2019-12-02 00:04:23 -0300

    adicionado startbat

---
+++ start.bat
@@ -0,0 +1,18 @@
+@echo off
+
+choco -v
+IF %ERRORLEVEL% NEQ 0 (
+    echo errorlevel %ERRORLEVEL%
+    @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+)
+
+call npm -v
+IF %ERRORLEVEL% NEQ 0 (
+    echo errorlevel %ERRORLEVEL%
+    choco install npm
+)
+
+call npm i -g npm
+npx http-server ./dist/browser -c-1 -p 8889
+cd..
+pause
