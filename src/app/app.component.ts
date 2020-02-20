cdf2009e1be3afedb02ecbdcaf637406caeb621e
Thomas O. Adriano <thomas.o.adriano@gmail.com>
2020-02-20 20:22:44 -0300

    adicionado blacklist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,26 @@
 import {
   Component,
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   AfterViewInit
-} from '@angular/core';
-import { LoadingService } from './core/loading/loading.service';
+} from "@angular/core";
+import { LoadingService } from "./core/loading/loading.service";
+import { BehaviorSubject } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements AfterViewInit {
-  public loading: boolean;
-  public title = 'buying-order-agent-frontend';
+  public loading$ = new BehaviorSubject(false);
+  public title = "buying-order-agent-frontend";
 
-  constructor(
-    private loadingService: LoadingService,
-    private cdr: ChangeDetectorRef
-  ) {}
+  constructor(private loadingService: LoadingService) {}
 
   public ngAfterViewInit(): void {
     this.loadingService.loadingChange().subscribe(l => {
-      if (this.loading !== l) {
-        this.loading = l;
-        console.log(this.loading);
-        this.cdr.detectChanges();
-      }
+      this.loading$.next(l);
     });
   }
 }
