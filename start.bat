532663999b434fec78e09c34a0b8f66228e2496d
Thomas O. Adriano <thomas.o.adriano@gmail.com>
2020-08-22 01:41:33 -0300

    ajustes

--- start.bat
+++ start.bat
@@ -1,23 +1,3 @@
 @echo off
-
-IF "%1" == "--update" (set UPDATE_TOOLS=true) else (set UPDATE_TOOLS=false)
-
-choco -v
-IF %ERRORLEVEL% NEQ 0 (
-    echo errorlevel %ERRORLEVEL%
-    @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-)
-
-call npm -v
-IF %ERRORLEVEL% NEQ 0 (
-    echo errorlevel %ERRORLEVEL%
-    choco install nodejs
-)
-
-if "%UPDATE_TOOLS%" == "true" (
-    call npm i -g npm
-)
-
 npx local-web-server --spa index.html --directory ./browser
 cd..
-pause
